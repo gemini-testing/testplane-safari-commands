57ecb153ab3f0d3135e590cf0320935ed79d77b0
DudaGod <dudkevich@yandex-team.ru>
2020-06-27 00:27:47 +0300

    fix(orientation) : do not throw if "executionContext" is not inited yet

--- test/lib/command-helpers/test-context.js
+++ test/lib/command-helpers/test-context.js
@@ -27,16 +27,33 @@ describe('"test-context" helper', () => {
             assert.equal(ctx, browser.executionContext.ctx.currentTest);
         });
 
-        it('should return test context for not hook', () => {
-            browser.executionContext = {some: 'test'};
+        [
+            {
+                name: 'not hook',
+                executionContext: {some: 'test'}
+            },
+            {
+                name: 'falsy value',
+                executionContext: undefined
+            }
+        ].forEach(({name, executionContext}) => {
+            it(`shoult return passed context for ${name}`, () => {
+                browser.executionContext = executionContext;
 
-            const ctx = testCtx.getTestContext(browser.executionContext);
+                const ctx = testCtx.getTestContext(browser.executionContext);
 
-            assert.equal(ctx, browser.executionContext);
+                assert.equal(ctx, browser.executionContext);
+            });
         });
     });
 
     describe('"resetTestContextValues" method', () => {
+        it('should not throw if test context is falsy', () => {
+            browser.executionContext = undefined;
+
+            assert.doesNotThrow(() => testCtx.resetTestContextValues(browser.executionContext, ['foo', 'bar']));
+        });
+
         it('should reset value in test context for one passed key', () => {
             browser.executionContext = {foo: 'value'};
 
